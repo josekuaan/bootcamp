a60f6db34465522feb171c5c61e755c53bf715bc
Grey22 <klsammie24@gmail.com>
2020-08-19 23:33:10 +0100

    fix route issue

--- DB/DBConnect.js
+++ DB/DBConnect.js
@@ -1,26 +1,23 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config({ path:"../config/config.env" });
+dotenv.config({ path: "../config/config.env" });
 
 let url;
-if(process.env.NODE_ENV === "development"){
-    url = "mongodb://localhost:27017/devcamper2"
-}else{
-   url=process.env.MONGODB_URI
+if (process.env.NODE_ENV === "development") {
+  url = "mongodb://localhost:27017/devcamper2";
+} else {
+  url = process.env.MONGODB_URI;
 }
 
-  
-     
-const connectDB = async () =>{
-   const conn = await mongoose.connect( "mongodb://localhost:27017/devcamper2", {
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useFindAndModify:false,
-        useUnifiedTopology:true 
-    })
-    console.log(`db connected to ${conn.connection.host}`)
-}      
-
+const connectDB = async () => {
+  const conn = await mongoose.connect("mongodb://localhost:27017/devcamper2", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`db connected to ${conn.connection.host}`);
+};
 
 module.exports = connectDB;
