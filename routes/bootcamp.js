dd817757894ceb4e10adda02b8d3c4bc24eda8a1
Grey22 <klsammie24@gmail.com>
2020-08-20 00:19:47 +0100

    fix route issue

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -8,7 +8,7 @@ const {
   deletebootCamp,
   getbootcampsInRadius,
   uploadBootcampPhoto,
-} = require("./controller/bootcamp.js");
+} = require("../controller/auth");
 const { protect, authorize } = require("../middleware/auth.js");
 
 //Include other resourse router
